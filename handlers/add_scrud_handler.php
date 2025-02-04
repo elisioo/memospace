5e534f28a1c608afe6c846af8a03b288493aac70
elisioo <e.sorono.544027@umindanao.edu.ph>
2025-02-04 23:34:58 +0800

    Update add_scrud_handler.php

--- handlers/add_scrud_handler.php
+++ handlers/add_scrud_handler.php
@@ -5,10 +5,11 @@ try {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $caption = $_POST['caption'];
         $description = $_POST['description'];
+        $img_dir = $_FILE['img_dir']['name'];
         $time = date("Y-m-d H:i:s");
 
-        $stmt = $conn->prepare("INSERT INTO simple_crud (caption, description, time) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $caption, $description, $time);
+        $stmt = $conn->prepare("INSERT INTO simple_crud (caption, description, img_dir, time) VALUES (?, ?, ?,?)");
+        $stmt->bind_param("sss", $caption, $description, $img_dir, $time);
 
         if ($stmt->execute()) {
             header("Location: ../simple-crud/uploads.php");
